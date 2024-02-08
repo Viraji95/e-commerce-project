f79c50741410c9de17ef3ddd2a38a52f32baa726
Virajisesiri <viraji.sesiri@gmail.com>
2024-02-08 11:01:31 +0530

    Initial commit

---
+++ back-end /src/test/java/lk/ijse/dep11/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
